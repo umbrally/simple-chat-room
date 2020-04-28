e50e71b5d4682b80869e39fdfee044ae6c7d0b9a
Jung Liu <realumbrally@gmail.com>
2020-04-28 17:14:14 +0800

    'initial-the-project'

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NotFound from './views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
