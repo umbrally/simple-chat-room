48499fd69a61395444dbd7cfe480396920c997a5
Jung Liu <realumbrally@gmail.com>
2020-06-02 17:06:34 +0800

    deploy

--- Vue.config.js
+++ Vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "publicPath": "./",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/simple-chat-room-vue/' : "./",
   "transpileDependencies": [
     "vuetify"
   ]
